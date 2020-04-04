9b137cdb42521c15567684ed444ed78e0231e133
foklan <gm.foklan@gmail.com>
2020-04-04 19:11:10 +0200

    Testing compress on rpi

--- backup_script.py
+++ backup_script.py
@@ -2,5 +2,33 @@ import subprocess
 
 class Backup:
     def __init__(self):
-        self.backup_from = None
-        self.backup_to = None
+        self.backup_from = "/home/"
+        self.backup_to = "/opt/scripts/new_backup/Raspberry-Pi-Backup/"
+        self.backup_name = "NEW-BACKUP-RPi3.tar.gz"
+        self.move_to = None
+        self.backup_script_folder = None
+        self.backup_log = None
+        self.do_backup = None
+        self.ping_counter = 100
+        self.do_shutdown = False
+        self.mac_address_of_nas = "48:0f:cf:33:e3:aa"
+
+    def start_nas(self):
+        subprocess.call(["wakeonlan", self.mac_address_of_nas], shell=True)
+
+    def compress_folders(self):
+        print("Executing LOCAL backup process...")
+        subprocess.call(["tar", "-cvzf", self.backup_to+self.backup_name], shell=True)
+
+    def pinger(self):
+        pass
+
+    def move_zip_to_nas(self):
+        pass
+
+    def start(self):
+        self.start_nas()
+        self.compress_folders()
+
+backup = Backup()
+backup.start()
